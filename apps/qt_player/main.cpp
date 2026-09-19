a9ab4ddc2a94a02f83aa829303c86732de43a8c2
huxiaoyun <2192438141@qq.com>
2026-09-19 21:26:11 +0800

    test: add application smoke startup check

--- apps/qt_player/main.cpp
+++ apps/qt_player/main.cpp
@@ -1,13 +1,23 @@
 ﻿#include "mainwid.h"
+#include "playback_runtime.h"
 #include <QApplication>
 #include <QFontDatabase>
 #include <QDebug>
+#include <cstdlib>
+#include <cstring>
 //#undef main
 
 int main(int argc, char *argv[])
 {
 //    qDebug() << "123";
     QApplication a(argc, argv);
+    bool smokeTest = false;
+    for (int index = 1; index < argc; ++index) {
+        if (std::strcmp(argv[index], "--smoke-test") == 0) {
+            smokeTest = true;
+            break;
+        }
+    }
     QCoreApplication::setApplicationName("MyPlayer");
     QCoreApplication::setApplicationVersion("1.0.0");
     QCoreApplication::setOrganizationName("MyPlayer");
@@ -15,6 +25,12 @@ int main(int argc, char *argv[])
     //使用第三方字库，用来作为UI图片 ://res/fa-solid-900.ttf
     QFontDatabase::addApplicationFont("://res/fontawesome-webfont.ttf");
     //QFontDatabase::addApplicationFont("://res/fa-solid-900.ttf");
+
+    if (smokeTest)
+    {
+        auto runtime = PlaybackRuntime::Create();
+        std::_Exit(runtime ? EXIT_SUCCESS : EXIT_FAILURE);
+    }
 
     MainWid w;
     if (w.Init() == false)
